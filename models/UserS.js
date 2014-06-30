4304016f21764db588e8b8bd7e82eb77001fcc2f
Ozgen Muzac <ozgen.muzac@comodo.com>
2014-06-30 14:45:27 +0000

    Removed redundants.

--- models/UserS.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    User: function(name, kimlikno, password, ip, mac) {
-        this.name = name;
-        this.kimlikno = kimlikno;
-        this.password = password;
-        this.ip = ip;
-        this.mac = mac
-    }
-}
